c2691eab93bd24eefae92f37c59db806a04a6dd1
unknown <jesusgg.b1@gmail.com>
2021-08-01 19:47:24 -0500

    React app update

--- torneo_de_debate/src/App.js
+++ torneo_de_debate/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/header"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
-    <div>Hello World</div>
+    <Header></Header>
   );
 }
 
